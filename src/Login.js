667fc555ccb618904548a7bdd4c138468fa1646a
Prince Sumberia <broughtmeup@gmail.com>
2020-05-26 20:30:13 +0530

    Add login page

---
+++ src/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div>
+      <h4 className="heading">Please Login With your Spotify Account.</h4>
+      <button>Login with Spotify</button>
+    </div>
+  );
+}
+
+export default Login;
