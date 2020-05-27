25a26782a75c89de16554223cb43ef3dbb897674
Prince Sumberia <broughtmeup@gmail.com>
2020-05-27 15:01:47 +0530

    Initial spotify auth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import "./App.scss";
+import Login from "./Login";
 
 let id = process.env.REACT_APP_CLIENT_ID;
 let sec = process.env.REACT_APP_CLIENT_SECRET;
@@ -8,7 +9,6 @@ let sec = process.env.REACT_APP_CLIENT_SECRET;
 let userAccessToken;
 
 function App() {
-  console.log(id, sec);
   const getData = async () => {
     const response = await axios.get("https://api.spotify.com/v1", {
       headers: {
@@ -23,6 +23,7 @@ function App() {
       <h1 className="heading">Hello World</h1>
       <p className="para">this is a sample para</p>
       <button onClick={getData}>Fetch</button>
+      <Login />
     </div>
   );
 }
--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
+
+let CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+
+let scope = "user-read-private user-read-email user-read-playback-state";
+const localhost = "http://localhost:3000/callback";
+
+let URL = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&redirect_uri=${localhost}&scope=${scope}&response_type=token`;
+
+// Get the hash of the url
+let hash = window.location.hash
+  .substring(1)
+  .split("&")
+  .reduce(function (initial, item) {
+    if (item) {
+      var parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+    }
+    return initial;
+  }, {});
+
+window.location.hash = "";
+
+let token = "";
 
 function Login() {
+  useEffect(() => {
+    token = hash.access_token;
+    const getData = async () => {
+      const response = await axios.get("https://api.spotify.com/v1/me", {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      });
+      return response.data;
+    };
+    console.log(getData().then((data) => console.log(data)));
+  }, []);
   return (
     <div>
       <h4 className="heading">Please Login With your Spotify Account.</h4>
-      <button>Login with Spotify</button>
+      <a href={URL}>Login with Spotify</a>
     </div>
   );
 }
