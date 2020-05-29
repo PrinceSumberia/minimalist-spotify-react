aae53a77f669b370876eb6cb6364a7793fac80fc
Prince Sumberia <broughtmeup@gmail.com>
2020-05-30 02:49:18 +0530

    refactor vode

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -16,13 +16,13 @@ export default function Dashboard() {
           Authorization: "Bearer " + accessToken,
         },
       });
-      return response;
-    };
-    getData().then((response) => {
       if (response.status === 200) {
         setProfileData(response.data);
+      } else {
+        console.log("error occured");
       }
-    });
+    };
+    getData();
   }, [accessToken, setProfileData]);
 
   return (
@@ -34,30 +34,3 @@ export default function Dashboard() {
     </div>
   );
 }
-
-// useEffect(() => {
-//   setAccessToken(hash.access_token);
-//   const getData = async () => {
-//     const response = await axios.get("https://api.spotify.com/v1/me", {
-//       headers: {
-//         Authorization: "Bearer " + accessToken,
-//       },
-//     });
-//     return response;
-//   };
-//   getData().then((result) => {
-//     if (result.status === 200) {
-//       console.log(result.data);
-//       setIsAuthenticated(true);
-//       history.push("/dashboard");
-//     } else {
-//       console.log("error occured");
-//     }
-//   });
-//   // if (accessToken) {
-//   //   console.log(accessToken);
-//   //   setIsAuthenticated(true);
-//   //   history.push("/dashboard");
-//   // }
-//   // setAccessToken(getData().then((data) => console.log(data)));
-// });
