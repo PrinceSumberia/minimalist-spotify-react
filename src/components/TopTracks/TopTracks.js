64648f3f369e7c7d0d3e023133792077a57d8e7f
Prince Sumberia <broughtmeup@gmail.com>
2020-06-01 22:49:31 +0530

    Add TopCharts

--- src/components/TopTracks/TopTracks.js
+++ src/components/TopTracks/TopTracks.js
@@ -1,26 +1,33 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { GLOBAL_TRACK_ID } from "../../constants/constants";
-import { DataContext } from "../../context/DataContext";
+import { DataContext, CurrentPlayListContext } from "../../context/DataContext";
 import useFetchData from "../../hooks/useFetchData";
 
 function TopTracks() {
   const { accessToken } = useContext(DataContext);
+  const { currentPlayList, setCurrentPlayList } = useContext(
+    CurrentPlayListContext
+  );
   const url = `https://api.spotify.com/v1/playlists/${GLOBAL_TRACK_ID}/tracks`;
   const headers = {
     Authorization: "Bearer " + accessToken,
   };
-
   const [{ data }] = useFetchData("", url, headers);
-  let trackList;
-  try {
-    trackList = data.items.map((track) => {
-      return track.track.id;
-    });
-  } catch (err) {}
+
+  useEffect(() => {
+    try {
+      const trackList = data.items.map((track) => {
+        return track.track.id;
+      });
+      setCurrentPlayList(trackList);
+    } catch (err) {}
+  }, [data, setCurrentPlayList]);
+
+  console.log(currentPlayList);
 
   return (
     <div>
-      <pre>{JSON.stringify(trackList, null, 2)}</pre>
+      <div className="">Hello</div>
     </div>
   );
 }
