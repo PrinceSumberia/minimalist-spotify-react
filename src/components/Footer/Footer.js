5a9b087a9b0c64b6dca0eac7237d119225b1aaa5
Prince Sumberia <broughtmeup@gmail.com>
2020-06-09 19:39:03 +0530

    Add and Style Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,27 +2,40 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import "./FooterStyles.scss";
+import bmac from "../../assets/bmac.svg";
 
 function Footer() {
   return (
     <footer className="footer">
-      <p className="footer__title">
+      <div className="footer__title">
         Made with <FontAwesomeIcon icon={faHeart} className="footer__heart" />
         by
-        <div className="footer__username">
+        <span className="footer__username">
           <a href="http://www.princesumberia.com" className="footer__link">
             Prince Sumberia
           </a>
+        </span>
+      </div>
+      <div className="footer__container">
+        <div>
+          <a
+            href="https://github.com/PrinceSumberia/minimalist-spotify-react"
+            className="btn btn-github footer__link"
+          >
+            Contribute on Github
+          </a>
         </div>
-      </p>
-      <p className="footer__container">
-        <a href="https://www.github.com/" className="btn footer__link">
-          Contribute on Github
-        </a>
-        <a href="https://www.google.com" className="btn footer__link">
-          Buy Me a Coffee
-        </a>
-      </p>
+        <div>
+          <a
+            href="https://www.buymeacoffee.com/princesumberia"
+            target="_blank"
+            className="footer__link"
+            rel="noopener noreferrer"
+          >
+            <img src={bmac} className="logo" alt="" />
+          </a>
+        </div>
+      </div>
     </footer>
   );
 }
