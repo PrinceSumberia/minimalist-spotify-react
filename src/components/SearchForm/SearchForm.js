76f9cd966abd7d47e3bf13a83e607bb6706894a8
Prince Sumberia <broughtmeup@gmail.com>
2020-06-15 19:03:34 +0530

    Add SearchForm and useInputState hooks

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,14 +1,22 @@
 import React from "react";
 import "./SearchFormStyles.scss";
 import { Search } from "react-feather";
+import useInputState from "../../hooks/useInputState";
 
 function SearchForm() {
+  const [searchInput, handleSearchInput, resetSearchInput] = useInputState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(searchInput);
+    resetSearchInput();
+  }
   return (
-    <form className="search">
-      <input name="" id="" className="search__input" placeholder="Search..." />
-      <div className="search__button">
+    <form className="search" onSubmit={handleSubmit}>
+      <input value={searchInput} className="search__input" placeholder="Search..." onChange={handleSearchInput} />
+      <button className="search__button">
         <Search className="search__icon" />
-      </div>
+      </button>
     </form>
   );
 }
---
+++ src/hooks/useInputState.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useState } from 'react'
+
+export default (initialValue = '') => {
+  const [value, setValue] = useState(initialValue)
+
+  const handleChange = (e) => {
+    setValue(e.target.value)
+  }
+
+  const reset = () => {
+    setValue("");
+  };
+
+  return [value, handleChange, reset]
+}
