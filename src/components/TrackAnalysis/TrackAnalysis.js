938a43b4ccb8c3311daf6a33ad7eed19862948fe
Prince Sumberia <broughtmeup@gmail.com>
2020-06-25 15:54:13 +0530

    Add recharts library

--- src/components/TrackAnalysis/TrackAnalysis.js
+++ src/components/TrackAnalysis/TrackAnalysis.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { DataContext } from "../../context/DataContext";
 import useFetchData from "../../hooks/useFetchData";
 
@@ -14,8 +13,13 @@ function TrackAnalysis({ match }) {
   };
 
   const [data] = useFetchData("", url, headers);
+
+  if (data.success) {
+    console.log(data.data);
+  }
+
   // const [data2] = useFetchData("", url2, headers);
-  console.log(data.data);
+  // console.log(data);
   return (
     <div>
       <h1>Hello World</h1>
@@ -24,3 +28,22 @@ function TrackAnalysis({ match }) {
 }
 
 export default TrackAnalysis;
+
+// acousticness: 0.247;
+// analysis_url: "https://api.spotify.com/v1/audio-analysis/7ytR5pFWmSjzHJIeQkgog4";
+// danceability: 0.746;
+// duration_ms: 181733;
+// energy: 0.69;
+// id: "7ytR5pFWmSjzHJIeQkgog4";
+// instrumentalness: 0;
+// key: 11;
+// liveness: 0.101;
+// loudness: -7.956;
+// mode: 1;
+// speechiness: 0.164;
+// tempo: 89.977;
+// time_signature: 4;
+// track_href: "https://api.spotify.com/v1/tracks/7ytR5pFWmSjzHJIeQkgog4";
+// type: "audio_features";
+// uri: "spotify:track:7ytR5pFWmSjzHJIeQkgog4";
+// valence: 0.497;
