83c5d5be7ebb14d0d592d64c3a6888b9a31faac6
Prince Sumberia <broughtmeup@gmail.com>
2020-06-27 21:42:01 +0530

    Change redirect url

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -4,8 +4,8 @@ export const GLOBAL_TRACK_ID = "37i9dQZEVXbMDoHDwVN2tF";
 const PERMISSION_SCOPE =
   "streaming user-modify-playback-state user-read-private user-read-email user-read-playback-state user-top-read";
 
-// const REDIRECT_URL = "https://minimalistspotify.netlify.app/";
-const REDIRECT_URL = "http://localhost:3000/";
+const REDIRECT_URL = "https://minimalistspotify.netlify.app/";
+// const REDIRECT_URL = "http://localhost:3000/";
 
 export const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL}&scope=${PERMISSION_SCOPE}&response_type=token&show_dialog={true}`;
 
