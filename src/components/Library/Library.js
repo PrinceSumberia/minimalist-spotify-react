719888c2fe88c7d77868184cce1e061f09d563b0
Prince Sumberia <broughtmeup@gmail.com>
2020-07-02 17:06:27 +0530

    Add remove song ability

--- src/components/Library/Library.js
+++ src/components/Library/Library.js
@@ -4,52 +4,96 @@ import { DataContext } from "../../context/DataContext";
 import { ArrowLeft } from "react-feather";
 import { millisToMinutesAndSeconds } from "../../utils/helpers";
 import Song from "../Song/Song";
+import { useEffect } from "react";
+import { useState } from "react";
+import Axios from "axios";
 
 function Library() {
   const { accessToken } = useContext(DataContext);
+  const [songList, setSongList] = useState([]);
   const headers = {
     Authorization: "Bearer " + accessToken,
   };
-  const [data] = useFetchData(
+  const [{ data }] = useFetchData(
     "",
     "https://api.spotify.com/v1/me/tracks",
     headers
   );
 
-  let list, songList;
+  useEffect(() => {
+    try {
+      let dataList = data.items.map((item) => {
+        const { id, name, artists, duration_ms, explicit, uri } = item.track;
+        const images = item.track.album.images;
+        const index = name.search(/\(/);
+        return {
+          id,
+          name: (index !== -1 ? name.slice(0, index) : name)
+            .trim()
+            .toLowerCase(),
+          thumbnail: images[images.length - 1].url,
+          image: images[1].url,
+          artist: artists
+            .map((artist) => artist.name)
+            .join(", ")
+            .toLowerCase(),
+          duration: millisToMinutesAndSeconds(duration_ms),
+          duration_ms,
+          explicit,
+          isLiked: true,
+          uri,
+        };
+      });
+      setSongList(dataList);
+    } catch (err) {}
+  }, [data]);
 
-  try {
-    list = data.data.items.map((item) => {
-      const { id, name, artists, duration_ms, explicit, uri } = item.track;
-      const images = item.track.album.images;
-      const index = name.search(/\(/);
-      return {
-        id,
-        name: (index !== -1 ? name.slice(0, index) : name).trim().toLowerCase(),
-        thumbnail: images[images.length - 1].url,
-        image: images[1].url,
-        artist: artists
-          .map((artist) => artist.name)
-          .join(", ")
-          .toLowerCase(),
-        duration: millisToMinutesAndSeconds(duration_ms),
-        duration_ms,
-        explicit,
-        isLiked: true,
-        uri,
-      };
+  const handleLike = (id) => {
+    const removeFav = async () => {
+      const response = await Axios.delete(
+        "https://api.spotify.com/v1/me/tracks",
+        {
+          headers: {
+            Accept: "application/json",
+            Authorization: `Bearer ${accessToken}`,
+            "Content-Type": "application/json",
+          },
+          data: {
+            ids: [id],
+          },
+        }
+      );
+      if (response.status === 200) {
+        console.log("Song Removed From Favorites");
+      }
+    };
+
+    let updatedSongList = songList.map((song) => {
+      if (song.id === id) {
+        const updatedSong = { ...song, isLiked: !song.isLiked };
+        return updatedSong;
+      }
+      return song;
     });
-    songList = list.map((song) => <Song key={song.id} {...song} />);
-    // console.log(list);
-  } catch (err) {}
-  // handleLike = { handleLike };
+    setSongList(updatedSongList);
+
+    let removedSong = updatedSongList.filter((song) => song.id !== id);
+    setTimeout(() => {
+      removeFav();
+      setSongList(removedSong);
+    }, 1000);
+  };
+
+  let list = songList.map((song) => (
+    <Song key={song.id} {...song} handleLike={handleLike} />
+  ));
 
   return (
     <div className="library">
       <div className="library__header">
         <h2 className="library__title">Library</h2>
       </div>
-      <div className="library__song">{songList}</div>
+      <div className="library__song">{list}</div>
     </div>
   );
 }
