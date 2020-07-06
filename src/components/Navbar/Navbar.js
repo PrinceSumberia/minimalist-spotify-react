9e312546d0317f67e289ac385a591224e10b6320
Prince Sumberia <broughtmeup@gmail.com>
2020-07-07 00:23:08 +0530

    Add logout logic to navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,20 @@
-import React, { memo } from "react";
-import "./NavbarStyles.scss";
+import React, { memo, useContext } from "react";
 import { NavLink } from "react-router-dom";
+import { DataContext } from "../../context/DataContext";
+import "./NavbarStyles.scss";
 
 function Navbar() {
+  const { setIsAuthenticated, setAccessToken, sdkPlayer } = useContext(
+    DataContext
+  );
+
+  const handleLogout = () => {
+    window.localStorage.removeItem("accessToken");
+    sdkPlayer.disconnect();
+    setIsAuthenticated(false);
+    setAccessToken(null);
+  };
+
   return (
     <div className="navbar">
       <div className="navbar__item">
@@ -26,8 +38,8 @@ function Navbar() {
         </NavLink>
       </div>
 
-      <div className="navbar__item navbar__logout">
-        <NavLink to="/library" className="navbar__link">
+      <div className="navbar__item navbar__logout" onClick={handleLogout}>
+        <NavLink to="#" className="navbar__link">
           Logout
         </NavLink>
       </div>
